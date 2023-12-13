b190338977fbbf71545afed787056577c8b43d97
Lance <lanceirvingmanzanal@icloud.com>
2023-12-13 14:24:33 -0700

    new function called head

---
+++ head.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(testArr) {
+  return(testArr.shift());
+}
+
+assertEqual(head([1, 2, 3, 4]), 1);
+assertEqual(head(["Lighthouse Labs", "Lighthouse", "Labs"]), "Lighthouse Labs");
+assertEqual(head([]), 1);
