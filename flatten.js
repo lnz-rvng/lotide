0acd16a2eaedee86676c6c2ae976ef8a568935bf
Lance <lanceirvingmanzanal@icloud.com>
2023-12-15 20:14:45 -0700

    created a function that flatten an array

---
+++ flatten.js
@@ -0,0 +1,37 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEquals = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🔴🔴🔴Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const flatten = function(arr) {
+  const flattened = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      for (let j = 0; j < arr[i].length; j++) {
+        flattened.push(arr[i][j]);
+      }
+    } else {
+      flattened.push(arr[i]);
+    }
+  }
+  return flattened;
+};
+
+
+assertArraysEquals(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
