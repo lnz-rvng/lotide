2220ccc6f13f31e86bea29fd47219b03e6a0c67e
Lance <lanceirvingmanzanal@icloud.com>
2023-12-18 16:19:38 -0700

    implements a function to chech at which indices a char occur

---
+++ letterPositions.js
@@ -0,0 +1,41 @@
+const letterPositions = function(sentence) {
+  const results = {};
+
+  for (let i = 0; i < sentence.length; i++) {
+    const char = sentence[i];
+    console.log(char);
+
+    if (results[char]) {
+      results[char].push(i);
+    } else {
+      results[char] = [i];
+    }
+  }
+
+  return results;
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEquals = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🔴🔴🔴Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+assertArraysEquals(letterPositions("hello").e, [1]);
+assertArraysEquals(letterPositions("hello").h, [0]);
+assertArraysEquals(letterPositions("hello").l, [2, 3]);
+
