cb051fa09270773519def9a625671c8d80e8e276
Lance <lanceirvingmanzanal@icloud.com>
2023-12-19 18:11:54 -0700

    fixed the parameters

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -10,11 +10,11 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-const assertArraysEquals = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅Assertion Passed: ${arr1} === ${arr2}`);
+const assertArraysEquals = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`🔴🔴🔴Assertion Failed: ${arr1} !== ${arr2}`);
+    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
