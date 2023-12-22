51c794d2780b0d105cf06b366d3ae71c685260c2
Lance <lanceirvingmanzanal@icloud.com>
2023-12-21 19:19:28 -0700

    created a takeUntil function that collects elements from the input until specified condition is met

--- takeUntil.js
+++ takeUntil.js
@@ -1,5 +1,11 @@
 const takeUntil = function(array, callback) {
-  
+  const result = [];
+
+  for (const item of array) {
+    if (!callback(item)) {
+      result.push(item);
+    } else return result;
+  }
 };
 
 const eqArrays = function(arr1, arr2) {
@@ -24,10 +30,10 @@ const assertArraysEqual = function(actual, expected) {
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
 
 console.log('---');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
